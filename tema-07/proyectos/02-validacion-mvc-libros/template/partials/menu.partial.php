db69217eba65951684dd37ae8560b4d23a8ba88d
Javiirt <javivirodri3702@gmail.com>
2025-01-29 22:00:44 +0100

    Proyecto mvc libros validacion perfiles terminado

--- tema-07/proyectos/02-validacion-mvc-libros/template/partials/menu.partial.php
+++ tema-07/proyectos/02-validacion-mvc-libros/template/partials/menu.partial.php
@@ -1,9 +1,11 @@
 <!-- Navigation -->
+<!-- Menú principal del proyecto -->
 
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">MVC - Gestión Geslibros</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="#">MVC - Gestión Geslibro</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
+      aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarScroll">
@@ -14,13 +16,12 @@
       </ul>
       <div class="d-flex">
         <div class="collapse navbar-collapse" id="exCollapsingNavbar">
-              <ul class="nav navbar-nav flex-row justify-content-between ml-auto">
-                  <li class="nav-item"><a href="<?= URL ?>auth/login" class="nav-link active">Login</a></li>
-                  <li class="nav-item"><a href="<?= URL ?>auth/register" class="nav-link active">Register</a></li>
-              </ul>
+          <ul class="nav navbar-nav flex-row justify-content-between ml-auto">
+            <li class="nav-item"><a href="<?= URL ?>auth/login" class="nav-link active">Login</a></li>
+            <li class="nav-item"><a href="<?= URL ?>auth/register" class="nav-link active">Register</a></li>
+          </ul>
         </div>
       </div>
     </div>
   </div>
 </nav>
-
