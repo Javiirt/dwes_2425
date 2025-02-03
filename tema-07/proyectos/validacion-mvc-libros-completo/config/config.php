e369db6d4a65397eee7db5e99e565eb102bfe6e4
Javiirt <javivirodri3702@gmail.com>
2025-02-03 22:59:50 +0100

    Proyecto mvc libro completo

---
+++ tema-07/proyectos/validacion-mvc-libros-completo/config/config.php
@@ -0,0 +1,16 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+
+define('URL', 'http://localhost/dwes/tema-07/proyectos/validacion-mvc-libros-completo/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'geslibros');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+
+
+?>
