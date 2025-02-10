7bb44e47e1c0a940b7172d1f022801255f18d423
Javiirt <javivirodri3702@gmail.com>
2025-02-10 20:35:50 +0100

    Comienzo proyecto 8.1

---
+++ tema-08/proyecto/gestor_album_01/config/config.php
@@ -0,0 +1,16 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+
+define('URL', 'http://localhost/dwes/tema-08/proyecto/gestor_album_01/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'fp');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+
+
+?>
---
+++ tema-08/proyecto/gestor_album_01/template/partials/footer.partial.php
@@ -0,0 +1,8 @@
+<!-- Footer -->
+
+<footer class="footer mt-auto py-3 fixed-bottom bg-light">
+  <div class="container">
+    <span class="text-muted">© 2025 
+        Javier Rodríguez Torres - DWES - 2º DAW - Curso 24/25</span>
+  </div>
+</footer>
