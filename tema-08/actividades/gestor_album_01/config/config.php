fede28edbf84b4d6f2232ba7c536b55f9436eb21
Javiirt <javivirodri3702@gmail.com>
2025-02-13 14:36:16 +0100

    actividad libros_csv

---
+++ tema-08/actividades/gestor_album_01/config/config.php
@@ -0,0 +1,16 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+
+define('URL', 'http://localhost/dwes/tema-08/actividades/gestor_album_01/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'album');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+
+
+?>
