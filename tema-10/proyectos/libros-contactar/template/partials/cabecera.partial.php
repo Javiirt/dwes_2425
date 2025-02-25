6d04172ae0726588f4f362233952f74c32da9929
Javiirt <javivirodri3702@gmail.com>
2025-02-25 10:32:51 +0100

    Proyecto libros email

---
+++ tema-10/proyectos/libros-contactar/template/partials/cabecera.partial.php
@@ -0,0 +1,12 @@
+<header>
+    <hgroup>
+        <!-- Titulos y subtitulos -->
+        <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h4 class="display-7">Proyecto Libros PDF </h4>
+            <p class="lead">Tema 9 - DWES - Curso 24/25</p>
+            <p>PDF</p>
+        </div>
+        </div>
+    </hgroup>
+</header>
